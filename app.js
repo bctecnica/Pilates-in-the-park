70fcd3c75cd396ebf4a079afbab2c58d49303e1e
ben <jammydude7@googlemail.com>
2022-07-04 14:13:59 +0100

    added slide panel nav for mobile

--- app.js
+++ app.js
@@ -42,3 +42,16 @@ generateProductCards();
 more.addEventListener("click", () => {
     generateProductCards();
 });
+
+
+
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "210px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
